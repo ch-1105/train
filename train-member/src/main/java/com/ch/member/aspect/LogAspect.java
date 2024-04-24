27af5ebd51fbea5e1af2a763f3231cb0b6f396d9
ch <ch0511@aliyun.com>
2024-04-24 18:54:25 +0800

    aop日志环绕

---
+++ train-member/src/main/java/com/ch/member/aspect/LogAspect.java
@@ -0,0 +1,98 @@
+package com.ch.member.aspect;
+
+/**
+ * author: ch
+ * create: 2024--2416:09
+ * Description:
+ */
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 系统日志切面
+ */
+@Aspect
+@Component
+public class LogAspect {
+    public LogAspect() {
+        System.out.println("Common LogAspect");
+    }
+    private static final Logger log = LoggerFactory.getLogger(LogAspect.class);
+
+    /**
+     * 这里我们使用注解的形式
+     * 当然，我们也可以通过切点表达式直接指定需要拦截的package,需要拦截的class 以及 method
+     * 切点表达式:   execution(public 所有返回类型 com.ch包下所有controller.所有方法（所有返回值）)
+     */
+
+    @Pointcut("execution(public * com.ch..*Controller.*(..))")
+    public void logPointCut() {
+    }
+
+    @Before("logPointCut()")
+    public void doBefore(JoinPoint joinPoint) {
+
+        // 开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        // 打印请求信息
+        log.info("------------- 开始 -------------");
+        log.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        log.info("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
+        log.info("远程地址: {}", request.getRemoteAddr());
+
+        // 打印请求参数
+        Object[] args = joinPoint.getArgs();
+        // log.info("请求参数: {}", JSONObject.toJSONString(args));
+
+        // 排除特殊类型的参数，如文件类型
+        Object[] arguments = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+        String[] excludeProperties = {};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        log.info("请求参数: {}", JSONObject.toJSONString(arguments, excludefilter));
+    }
+
+    @Around("logPointCut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+        String[] excludeProperties = {};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        log.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
+        log.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+}
+
