f33714e495c6d6d46628fb96dddb4d12f0322671
ch <ch0511@aliyun.com>
2024-05-25 00:07:50 +0800

    解决浏览器刷新后token丢失

--- train-web/src/store/index.js
+++ train-web/src/store/index.js
@@ -1,14 +1,17 @@
 import { createStore } from 'vuex'
+const Member = "Member";
 
 export default createStore({
+
   state: {
-    member:{}
+    member:window.SessionStorage.get(Member) || {}
   },
   getters: {
   },
   mutations: {
     setMember (state,_member){
       state.member = _member
+      window.SessionStorage.set(Member,_member)
     }
   },
   actions: {
