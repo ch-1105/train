3e91f49624d86b9554cc13d4e5a3d3e932de014a
ch <ch0511@aliyun.com>
2024-05-25 10:46:48 +0800

    添加拦截器，实现拦截登录校验

---
+++ train-gateway/src/main/java/com/ch/gateway/config/JwtUtil.java
@@ -0,0 +1,79 @@
+package com.ch.gateway.config;
+
+import cn.hutool.core.date.DateField;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.crypto.GlobalBouncyCastleProvider;
+import cn.hutool.json.JSONObject;
+import cn.hutool.jwt.JWT;
+import cn.hutool.jwt.JWTPayload;
+import cn.hutool.jwt.JWTUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * author: ch
+ * create: 2024--2423:10
+ * Description:
+ */
+public class JwtUtil {
+    private static final Logger log = LoggerFactory.getLogger(JwtUtil.class);
+
+    /**
+     * 盐值很重要，不能泄漏，且每个项目都应该不一样，可以放到配置文件中
+     */
+    private static final String key = "ch-12306";
+
+    public static String createToken(Long id, String mobile) {
+        log.info("开始生成JWT token，id：{}，mobile：{}", id, mobile);
+        GlobalBouncyCastleProvider.setUseBouncyCastle(false);
+        DateTime now = DateTime.now();
+        DateTime expTime = now.offsetNew(DateField.HOUR, 24);
+        Map<String, Object> payload = new HashMap<>();
+        // 签发时间
+        payload.put(JWTPayload.ISSUED_AT, now);
+        // 过期时间
+        payload.put(JWTPayload.EXPIRES_AT, expTime);
+        // 生效时间
+        payload.put(JWTPayload.NOT_BEFORE, now);
+        // 内容
+        payload.put("id", id);
+        payload.put("mobile", mobile);
+        String token = JWTUtil.createToken(payload, key.getBytes());
+        log.info("生成JWT token：{}", token);
+        return token;
+    }
+
+    public static boolean validate(String token) {
+        log.info("开始JWT token校验，token：{}", token);
+        GlobalBouncyCastleProvider.setUseBouncyCastle(false);
+        JWT jwt = JWTUtil.parseToken(token).setKey(key.getBytes());
+        // validate包含了verify
+        boolean validate = jwt.validate(0);
+        log.info("JWT token校验结果：{}", validate);
+        return validate;
+    }
+
+    public static JSONObject getJSONObject(String token) {
+        GlobalBouncyCastleProvider.setUseBouncyCastle(false);
+        JWT jwt = JWTUtil.parseToken(token).setKey(key.getBytes());
+        JSONObject payloads = jwt.getPayloads();
+        payloads.remove(JWTPayload.ISSUED_AT);
+        payloads.remove(JWTPayload.EXPIRES_AT);
+        payloads.remove(JWTPayload.NOT_BEFORE);
+        log.info("根据token获取原始内容：{}", payloads);
+        return payloads;
+    }
+
+    public static void main(String[] args) {
+        createToken(1L, "123");
+
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYmYiOjE3MTY1NjM3MjAsIm1vYmlsZSI6IjExMTAwMDAwMDAwIiwiaWQiOjE3OTM2NDA5NTIxMzMzNDUyODAsImV4cCI6MTcxNjY1MDEyMCwiaWF0IjoxNzE2NTYzNzIwfQ.Ft3dfFxyVKFwsvAe-73b5E5-MC4tNphldvFkm_RPc0c";
+        validate(token);
+
+        getJSONObject(token);
+    }
+}
+
