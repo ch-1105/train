b32b3013c25288c3f392427635f405bd47e095cc
ch <ch0511@aliyun.com>
2024-06-10 10:00:29 +0800

    test2

--- train-common/src/main/java/com/ch/common/aspect/test.java
+++ train-common/src/main/java/com/ch/common/aspect/test.java
@@ -1,9 +1,14 @@
 package com.ch.common.aspect;
 
+import com.ch.common.utils.JwtUtil;
+
 /**
  * author: ch
  * create: 2024--109:57
  * Description:
  */
 public class test {
+    public static void main(String[] args) {
+        System.out.println(JwtUtil.createToken(1L, ""));
+    }
 }
