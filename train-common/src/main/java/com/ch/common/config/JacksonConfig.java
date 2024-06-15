da53b9b4fb3f4f8712f23fecb3431ddeab109784
ch <ch0511@aliyun.com>
2024-06-15 16:01:14 +0800

    解决后端返回给前端Long精度丢失问题

---
+++ train-common/src/main/java/com/ch/common/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package com.ch.common.config;
+
+/**
+ * author: ch
+ * create: 2024--1515:52
+ * Description: 全局将long类型转换string给前端，会导致一些小问题
+ */
+//@Configuration
+public class JacksonConfig {
+
+//    @Bean
+//    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+//        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+//
+//        SimpleModule simpleModule = new SimpleModule();
+//        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+//        objectMapper.registerModule(simpleModule);
+//
+//        return objectMapper;
+//    }
+}
--- train-member/src/main/java/com/ch/member/responce/PassengerQueryResponse.java
+++ train-member/src/main/java/com/ch/member/responce/PassengerQueryResponse.java
@@ -1,8 +1,11 @@
 package com.ch.member.responce;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 乘车人列表查询
@@ -10,9 +13,19 @@ import java.io.Serializable;
 @Data
 public class PassengerQueryResponse implements Serializable {
 
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long id;
+
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long memberId;
+
     private String name;
 
     private String idCard;
 
     private String type;
+
+    private Date createTime;
+
+    private Date updateTime;
 }
