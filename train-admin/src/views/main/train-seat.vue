ef9cb7e1b7cab571e5fd19f10e98354baaca7662
ch <ch0511@aliyun.com>
2024-07-07 21:09:14 +0800

    车厢及座位页绑定组件

--- train-admin/src/views/main/train-seat.vue
+++ train-admin/src/views/main/train-seat.vue
@@ -42,7 +42,7 @@
            ok-text="确认" cancel-text="取消">
     <a-form :model="trainSeat" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="车次编号">
-        <a-input v-model:value="trainSeat.trainCode" />
+        <train-select-view v-model:value="trainSeat.trainCode" />
       </a-form-item>
       <a-form-item label="厢序">
         <a-input v-model:value="trainSeat.carriageIndex" />
@@ -76,9 +76,11 @@
 import { defineComponent, ref, onMounted } from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
+import TrainSelectView from "@/components/train-select-view.vue";
 
 export default defineComponent({
   name: "train-seat-view",
+  components: {TrainSelectView},
   setup() {
     const SEAT_COL = window.SEAT_COL;
     const SEAT_TYPE = window.SEAT_TYPE;
