9c76b77d0a42cca101f405acb19614fde90194eb
ch <ch0511@aliyun.com>
2024-07-11 13:32:46 +0800

    添加springboot 定时模块以及quartz依赖

---
+++ timing-scheduling/src/main/java/com/ch/timer/job/TestJob.java
@@ -0,0 +1,19 @@
+package com.ch.timer.job;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+/**
+ * author: ch
+ * create: 2024--1113:24
+ * Description:
+ */
+@Component
+@EnableScheduling
+public class TestJob {
+
+//    @Scheduled(cron = "0/10 * * * * ?")
+    public void test1() {
+        System.out.println("test1");
+    }
+}
