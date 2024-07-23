595fbf56d9353380698967ca967d4453b4def905
ch <ch0511@aliyun.com>
2024-07-23 09:33:34 +0800

    14.2 nacos多环境

--- train-member/src/main/java/com/ch/member/controller/TestController.java
+++ train-member/src/main/java/com/ch/member/controller/TestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 /**
  * author: ch
  * create: 2024--1914:31
@@ -21,6 +23,7 @@ public class TestController {
 
     @GetMapping("/nacos-test")
     public Result<String> test(){
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
         System.out.println("test : "+test);
         return Result.success(test);
     }
