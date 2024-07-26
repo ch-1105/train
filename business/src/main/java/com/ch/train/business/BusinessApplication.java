baaddfb6dc4ebf48170e3a11b9fefa124efa0723
ch <ch0511@aliyun.com>
2024-07-26 11:00:03 +0800

    17.4 先关闭main注册sentinel

--- business/src/main/java/com/ch/train/business/BusinessApplication.java
+++ business/src/main/java/com/ch/train/business/BusinessApplication.java
@@ -1,8 +1,5 @@
 package com.ch.train.business;
 
-import com.alibaba.csp.sentinel.slots.block.RuleConstant;
-import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
-import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @ComponentScan(basePackages = "com.ch")
@@ -31,19 +25,19 @@ public class BusinessApplication {
         logger.info("Ip地址：http://127.0.0.1:{}{}",
                 run.getEnvironment().getProperty("server.port"),
                 run.getEnvironment().getProperty("server.servlet.context-path"));
-        initFlowRules();
+//        initFlowRules();
     }
 
-    private static void initFlowRules(){
-        List<FlowRule> rules = new ArrayList<>();
-        FlowRule rule = new FlowRule();
-        rule.setResource("saveOrder");
-        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
-        // Set limit QPS to 20.
-        rule.setCount(1);
-        rules.add(rule);
-        FlowRuleManager.loadRules(rules);
-        logger.info("开始限流");
-    }
+//    private static void initFlowRules(){
+//        List<FlowRule> rules = new ArrayList<>();
+//        FlowRule rule = new FlowRule();
+//        rule.setResource("saveOrder");
+//        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+//        // Set limit QPS to 20.
+//        rule.setCount(1);
+//        rules.add(rule);
+//        FlowRuleManager.loadRules(rules);
+//        logger.info("开始限流");
+//    }
 
 }
